f6bb2372b03959ad78b2095159acf4e797e15641
densuns <bilowalrahman@gmail.com>
2026-09-10 15:54:39 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "The Lazy Bot",
-  botOwner: 'Professor', // Your name
+  botOwner: 'The Lazy Guy', // Your name
   ownerNumber: '8801756425604', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
